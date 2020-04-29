56247258b8180ba10c85be828f0d18e88a4334d9
the100rabh <gitkraken@the100rabh.otherinbox.com>
2020-04-30 03:18:52 +0530

    updating pwa file

--- lib/pwa/offline_urls.g.dart
+++ lib/pwa/offline_urls.g.dart
@@ -72,4 +72,4 @@ final List<String> offlineUrls = [
 ];
 
 /// Last modified timestamp of the files
-final String lastModified = '2020-04-29T10:08:21.106Z';
+final String lastModified = '2020-04-29T21:48:49.472Z';
